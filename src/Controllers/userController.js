b98cbde849e42b7e626c85cc51f2f9dd4909e0c7
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2025-05-04 13:15:34 +0530

    Add update profile code

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../Models/userModel.js";
 import { errorHandler } from "../Utils/errorHandler.js";
+import validator from "validator";
 
 export const updateProfilePic = async (req, res, next) => {
   try {
@@ -19,6 +20,80 @@ export const updateProfilePic = async (req, res, next) => {
     res.status(200).json({
       success: true,
       message: "Profile Pic updated successfully",
+      userData: updateUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateProfile = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+
+    const updateData = {};
+
+    /* Fullname */
+    if (req.body?.fullname) {
+      const fullname = req.body.fullname;
+
+      // Validate fullname
+      if (!validator.isAlphanumeric(fullname.replace(/\s/g, ""))) {
+        return next(
+          errorHandler(400, "Name can only contain strings, number and space")
+        );
+      }
+
+      // Add fullname to updateData object
+      updateData.fullname = fullname;
+    }
+
+    /* Email */
+    if (req.body.email) {
+      const email = req.body.email;
+
+      //  Validate email
+      if (!validator.isEmail(email)) {
+        return next(errorHandler(400, "Invalid email format"));
+      }
+
+      /* Add to update data object */
+      updateData.email = email;
+    }
+
+    /* Password */
+    if (req.body.password) {
+      const password = req.body.password;
+
+      // validate password
+      if (
+        !validator.isStrongPassword(password, {
+          minLength: 6,
+          minSymbols: 1,
+          minNumbers: 1,
+          minLowercase: 1,
+        })
+      ) {
+        return next(
+          errorHandler(
+            400,
+            "Password must be at least 6 character length and it must contain at least 1 lowercase,1 uppercase, 1 number and 1 symbol."
+          )
+        );
+      }
+      /* Add to update data object */
+      updateData.password = password;
+    } 
+
+    const updateUser = await User.findByIdAndUpdate(
+      userId,
+      { $set: updateData },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Profile Data updated successfully",
       userData: updateUser,
     });
   } catch (error) {
--- src/Routes/userRoutes.js
+++ src/Routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getUsers, updateProfilePic } from '../Controllers/userController.js';
+import { getUsers, updateProfile, updateProfilePic } from '../Controllers/userController.js';
 import { authMiddleware } from './../Middleware/authMiddleware.js';
 import upload from '../Config/multer.js';
 
 const route = express.Router();
 
-route.put("/update/profile-pic", authMiddleware, upload('profiles').single("profilePic"), updateProfilePic)
+route.patch("/update/profile-pic", authMiddleware, upload('profiles').single("profilePic"), updateProfilePic)
+route.put("/update/profile", authMiddleware, updateProfile)
 route.get("/users", authMiddleware, getUsers)
 
 export default route;
