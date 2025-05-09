84c042b958622705496e5c6a9bf24c6c61f20c48
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2025-05-09 11:57:14 +0530

    Update import path in navbar

--- src/Config/socket.js
+++ src/Config/socket.js
@@ -1,6 +1,9 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import { dotenv } from 'dotenv';
+
+dotenv.config();
 
 // Create the express app (dont create this again in server.js)
 const app = express();
@@ -10,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
   },
 });
 
